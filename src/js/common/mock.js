34f436451b12b07a8548237b71b8dea3277ba431
juxiezuo-cyh <422738768@qq.com>
2018-03-02 17:08:09 +0800

    first commit2

--- src/js/common/mock.js
+++ src/js/common/mock.js
@@ -1,1 +1,13 @@
 import FetchMock from 'fetch-mock';
+FetchMock.mock('/login', (url, opts) => {
+  const params = opts.params;
+  if (params.account === '18500101020') {
+    if (params.password === '12345') {
+      return {code: 200, message: 'success'};
+    }else {
+      return {code: 401, message: '密码错误'}
+    }
+  }else {
+    return {code: 400, message: '用户名错误'}
+  }
+});
